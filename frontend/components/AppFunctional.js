a04158b951c67bd86508647de0f43f4f58cc050a
emracatal <134175723+emracatal@users.noreply.github.com>
2023-08-28 17:32:34 +0300

    email alanının onchange eksik

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 // önerilen başlangıç stateleri
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
+const initialMessage = "";
+const initialEmail = "";
+const initialSteps = 0;
+const initialIndex = 4; //  "B" nin bulunduğu indexi
 
 export default function AppFunctional(props) {
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
-
+  const [message, setMessage] = useState(initialMessage);
+  const [email, setEmail] = useState(initialEmail);
+  const [steps, setSteps] = useState(initialSteps);
+  const [index, setIndex] = useState(initialIndex);
   function getXY() {
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
     // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
+    return `(${(index % 3) + 1}, ${Math.floor(index / 3) + 1})`;
   }
 
   function getXYMesaj() {
@@ -23,12 +28,54 @@ export default function AppFunctional(props) {
 
   function reset() {
     // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
+    setMessage(initialMessage);
+    setEmail(initialEmail);
+    setSteps(initialSteps);
+    setIndex(initialIndex);
   }
 
   function sonrakiIndex(yon) {
     // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
     // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
     // şu anki indeksi değiştirmemeli.
+    console.log(yon);
+    setMessage("");
+
+    if (yon === "left") {
+      if (index % 3 !== 0) {
+        setIndex(index - 1);
+        setSteps(steps + 1);
+      } else {
+        setMessage("Sola gidemezsiniz");
+      }
+    }
+
+    if (yon === "right") {
+      if (index % 3 !== 2) {
+        setIndex(index + 1);
+        setSteps(steps + 1);
+      } else {
+        setMessage("Sağa gidemezsiniz");
+      }
+    }
+
+    if (yon === "up") {
+      if (index > 2) {
+        setIndex(index - 3);
+        setSteps(steps + 1);
+      } else {
+        setMessage("Yukarıya gidemezsiniz");
+      }
+    }
+
+    if (yon === "down") {
+      if (index < 6) {
+        setIndex(index + 3);
+        setSteps(steps + 1);
+      } else {
+        setMessage("Aşağıya gidemezsiniz");
+      }
+    }
   }
 
   function ilerle(evt) {
@@ -38,41 +85,72 @@ export default function AppFunctional(props) {
 
   function onChange(evt) {
     // inputun değerini güncellemek için bunu kullanabilirsiniz
+    evt.preventDefault();
+    setEmail(evt.target.value);
   }
 
   function onSubmit(evt) {
+    evt.preventDefault();
     // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
+    axios
+      .post("/http://localhost:9000/api/result", {
+        email: email,
+        steps: steps,
+        ...getXY(),
+      })
+      .then(function (response) {
+        console.log(response.data);
+        setMessage(response.data.message);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setMessage(error.response.data.message);
+      });
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3 id="coordinates">Koordinatlar {getXY()}</h3>
+        <h3 id="steps">{steps} kere ilerlediniz</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div key={idx} className={`square${idx === index ? " active" : ""}`}>
+            {idx === index ? "B" : null}
+          </div>
+        ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button onClick={(e) => sonrakiIndex(e.target.id)} id="left">
+          SOL
+        </button>
+        <button onClick={(e) => sonrakiIndex(e.target.id)} id="up">
+          YUKARI
+        </button>
+        <button onClick={(e) => sonrakiIndex(e.target.id)} id="right">
+          SAĞ
+        </button>
+        <button onClick={(e) => sonrakiIndex(e.target.id)} id="down">
+          AŞAĞI
+        </button>
+        <button id="reset" onClick={reset}>
+          reset
+        </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="email girin"></input>
+      <form onSubmit={onSubmit}>
+        <input
+          id="email"
+          type="email"
+          placeholder="email girin"
+          value={email}
+          onChange={onChange}
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
